34cc12b76149e75cd4ef00a0540c17f82d79ceaa
MSiesto <miguelsiesto@gmail.com>
2023-01-18 13:48:25 +0100

    Creating and Styling Pause,Play,Settings Buttons

--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,8 @@
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import PauseButton from "./PauseButton";
+import PlayButton from "./PlayButton";
+import SettingsButton from "./SettingsButton";
 
 const red = "#f54e4e";
 const green = "#4aec8c";
@@ -15,7 +18,13 @@ function Timer() {
           tailColor: "rgba(255, 255, 255, .2)",
         })}
       />
-      <div></div>
+      <div style={{ marginTop: "20px" }}>
+        <PlayButton />
+        <PauseButton />
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        <SettingsButton />
+      </div>
     </div>
   );
 }
