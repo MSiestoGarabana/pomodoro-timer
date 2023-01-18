6dac427724eba4d573a65d3261a1bc6bf4d8244c
MSiesto <miguelsiesto@gmail.com>
2023-01-18 13:30:01 +0100

    create and style CircularProgressBar

--- src/Timer.js
+++ src/Timer.js
@@ -1,4 +1,22 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+const red = "#f54e4e";
+const green = "#4aec8c";
 function Timer() {
-  return <div>timer</div>;
+  return (
+    <div>
+      <CircularProgressbar
+        value={60}
+        text={`${60}%`}
+        styles={buildStyles({
+          textColor: "#fff",
+          pathColor: red,
+          tailColor: "rgba(255, 255, 255, .2)",
+        })}
+      />
+      <div></div>
+    </div>
+  );
 }
 export default Timer;
