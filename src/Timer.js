3bd13eaa6534c79b5740d1202dc7622cc92ee616
MSiesto <miguelsiesto@gmail.com>
2023-01-20 12:50:53 +0100

    creating countdown logic

--- src/Timer.js
+++ src/Timer.js
@@ -3,18 +3,75 @@ import "react-circular-progressbar/dist/styles.css";
 import PauseButton from "./PauseButton";
 import PlayButton from "./PlayButton";
 import SettingsButton from "./SettingsButton";
-import { useContext } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import SettingsContext from "./SettingsContext";
 
 const red = "#f54e4e";
 const green = "#4aec8c";
 function Timer() {
   const settingsInfo = useContext(SettingsContext);
+  const [isPaused, setIsPaused] = useState(false);
+  const [mode, setMode] = useState("work"); //work/break/null
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  const secondsLeftRef = useRef(secondsLeft);
+  const isPausedRef = useRef(isPaused);
+  const modeRef = useRef(mode);
+
+  function switchMode() {
+    const nextMode = modeRef.current === "work" ? "break" : "work";
+    const nextSeconds =
+      (nextMode === "work"
+        ? settingsInfo.workMinutes
+        : settingsInfo.breakMinutes) * 60;
+
+    setMode(nextMode);
+    modeRef.current = nextMode;
+
+    setSecondsLeft(nextSeconds);
+    secondsLeftRef.current = nextSeconds;
+  }
+
+  function tick() {
+    secondsLeftRef.current--;
+    setSecondsLeft(secondsLeftRef.current);
+  }
+
+  function initTimer() {
+    setSecondsLeft(secondsLeftRef.current);
+  }
+
+  useEffect(() => {
+    initTimer();
+
+    const interval = setInterval(() => {
+      if (isPausedRef.current) {
+        return;
+      }
+      if (secondsLeftRef.current === 0) {
+        return switchMode();
+      }
+
+      tick();
+    }, 1000);
+    return clearInterval(interval);
+  }, [settingsInfo]);
+
+  const totalSeconds =
+    mode === "work"
+      ? settingsInfo.workMinutes * 60
+      : settingsInfo.breakMinutes * 60;
+
+  const percentage = Math.round(secondsLeft / totalSeconds) * 100;
+  const minutes = Math.floor(secondsLeft / 60); //
+  let seconds = secondsLeft % 60;
+  if (seconds < 10) seconds = "0" + seconds;
+
   return (
     <div>
       <CircularProgressbar
-        value={60}
-        text={`${60}%`}
+        value={percentage}
+        text={minutes + ":" + seconds}
         styles={buildStyles({
           textColor: "#fff",
           pathColor: red,
@@ -22,8 +79,7 @@ function Timer() {
         })}
       />
       <div style={{ marginTop: "20px" }}>
-        <PlayButton />
-        <PauseButton />
+        {isPaused ? <PlayButton /> : <PauseButton />}
       </div>
       <div style={{ marginTop: "20px" }}>
         <SettingsButton
